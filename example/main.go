e76045e93824e3be8b94c6f98306c5f1a2297dc8
Meet Soni <meet.soni1513@gmail.com>
2023-03-08 14:35:17 +0530

    Random IPv4 address generator function

--- example/main.go
+++ example/main.go
@@ -68,4 +68,14 @@ func main() {
 		fmt.Println(arr)
 	}
 
+	fmt.Println(" ")
+	fmt.Println(" //IPv4 generator  ")
+	fmt.Println(" ")
+
+	//Random IP address v4 generator
+	for i := 0; i < 5; i++ {
+		// Here we have provided argument
+		arr := apace.RandomIPv4()
+		fmt.Println(arr)
+	}
 }
